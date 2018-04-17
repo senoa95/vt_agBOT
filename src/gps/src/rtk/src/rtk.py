3a95cd193f9b69d0a11414a267b29903e576e163
senoa95 <sena@vt.edu>
2018-04-17 01:16:13 -0400

    RTK fixed

--- src/gps/src/rtk/src/rtk.py
+++ src/gps/src/rtk/src/rtk.py
@@ -5,19 +5,24 @@ import serial
 import socket
 from socket import error as socket_error
 
-###RTK Connection Initialization
-
-TCP_IP = '192.168.0.60'
-TCP_PORT = 5017
+###Trimble TCP Connection Initialization
+TCP_IP = '192.168.1.4'
+TCP_PORT = 5017 #this is the port number inside the trimble configuration
 BUFFER_SIZE = 1024
 
+# Open TCP Socket
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((TCP_IP, TCP_PORT))
 
-#SETTING UP RTK
+# Open Serial connection
+ ser0 = serial.Serial('/dev/ttyUSB0')
+ ser1 = serial.Serial('/dev/ttyUSB1')
+ ser2 = serial.Serial('/dev/ttyUSB2')
+
+#SETTING UP Novatel to take RTCM correction
 ser0.write('INTERFACEMODE USB1 RTCM NOVATEL ON\r\n')
 ser1.write('INTERFACEMODE USB2 RTCM NOVATEL ON\r\n')
-ser2.write('INTERFACEMODE USB2 RTCM NOVATEL ON\r\n')
+ser2.write('INTERFACEMODE USB3 RTCM NOVATEL ON\r\n')
 
 
 while True:
