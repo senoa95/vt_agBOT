4c40f26f9caab413db8d24c8bbf7c0470db1428b
senoa95 <sena@vt.edu>
2018-04-17 00:41:33 -0400

    fixes to packages and cleanup.

--- build/ackermann_vehicle-1/ackermann_vehicle_description/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/agbot/vt_agBOT/src/ackermann_vehicle-1/ackermann_vehicle_description
-# Build directory: /home/agbot/vt_agBOT/build/ackermann_vehicle-1/ackermann_vehicle_description
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
