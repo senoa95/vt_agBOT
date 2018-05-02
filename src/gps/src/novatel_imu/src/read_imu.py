9a80331888aff299d406b84eb3ab5916ddeec7f0
senoa95 <sena@vt.edu>
2018-05-01 22:06:39 -0400

    Fixed bugs in imu_reader

--- src/gps/src/novatel_imu/src/read_imu.py
+++ src/gps/src/novatel_imu/src/read_imu.py
@@ -14,9 +14,6 @@ ser.write('LOG USB3 INSPVAA ONTIME 0.05\r\n')
 
 def read_imu():
     orientation = Point32()
-    roll = orientation.x
-    pitch = orientation.y
-    yaw = orientation.z
 
     imu_data_raw = ser.readline()
     imu_data = imu_data_raw.split(',')
@@ -25,11 +22,29 @@ def read_imu():
         pitch = float(imu_data[18])
         yaw = float(imu_data[19])
 
-    imu_pub = rospy.Publisher('/imu', Point32, queue_size=10)
+    imu_pub = rospy.Publisher('/novatel_imu', Point32, queue_size=10)
     rospy.init_node ('novatel_imu', anonymous=True)
     rate = rospy.Rate(10)
     while not rospy.is_shutdown():
+        imu_data_raw = ser.readline()
+        imu_data = imu_data_raw.split(',')
+        if len(imu_data) > 19:
+           roll = float(imu_data[17])
+           pitch = float(imu_data[18])
+           yaw = float(imu_data[19])
+
+        orientation.x = roll
+        orientation.y = pitch
+        orientation.z = yaw
+
         rospy.loginfo(orientation)
-        rospy.publish(orientation)
+        imu_pub.publish(orientation)
         rate.sleep()
+
+
+if __name__ == '__main__':
+    try:
+        read_imu()
+    except rospy.ROSInterruptException:
+        pass
 
