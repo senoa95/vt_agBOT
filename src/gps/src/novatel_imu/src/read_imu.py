ada2c2a140661c41d966e8886e91ec4089a46cbf
senoa95 <sena@vt.edu>
2018-05-05 22:40:15 -0400

    added a patch to fix novatel_imu buffer issue. Also removed not working imu package.

--- src/gps/src/novatel_imu/src/read_imu.py
+++ src/gps/src/novatel_imu/src/read_imu.py
@@ -9,12 +9,14 @@ ser = serial.Serial('/dev/ttyUSB2',115200,rtscts=True,dsrdtr=True)
 
 #SETTING UP Novatel to take RTCM correction
 ser.write('UNLOGALL USB3\r\n')
-ser.flush()
 ser.write('LOG USB3 INSPVAA ONTIME 0.05\r\n')
 
 def read_imu():
+    prev_roll = 0
+    prev_pitch = 0
+    prev_yaw = 0
     orientation = Point32()
-
+    ser.flush()
     imu_data_raw = ser.readline()
     imu_data = imu_data_raw.split(',')
     if len(imu_data) > 19:
@@ -22,16 +24,26 @@ def read_imu():
         pitch = float(imu_data[18])
         yaw = float(imu_data[19])
 
+
     imu_pub = rospy.Publisher('/novatel_imu', Point32, queue_size=10)
     rospy.init_node ('novatel_imu', anonymous=True)
     rate = rospy.Rate(10)
     while not rospy.is_shutdown():
+        ser.flush()
         imu_data_raw = ser.readline()
         imu_data = imu_data_raw.split(',')
         if len(imu_data) > 19:
-           roll = float(imu_data[17])
-           pitch = float(imu_data[18])
-           yaw = float(imu_data[19])
+           curr_roll = float(imu_data[17])
+           curr_pitch = float(imu_data[18])
+           curr_yaw = float(imu_data[19])
+
+           # fix to IMU buffer issue
+           if abs(curr_roll - prev_roll) > 20:
+               ser.flush()
+           if abs(curr_pitch - prev_pitch) > 20:
+               ser.flush()
+           if abs(curr_yaw - prev_yaw) > 20:
+               ser.flush()
 
         orientation.x = roll
         orientation.y = pitch
@@ -47,4 +59,3 @@ if __name__ == '__main__':
         read_imu()
     except rospy.ROSInterruptException:
         pass
-
