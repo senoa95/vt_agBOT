d921e894452ec03140d5386fd1771845f86d0625
senoa95 <sena@vt.edu>
2018-05-07 19:26:30 -0400

    GPS recovered with the parsing problem in the IMU fixed by syncing log clock with serial.

--- src/gps/src/novatel_imu/src/read_imu.py
+++ src/gps/src/novatel_imu/src/read_imu.py
@@ -15,43 +15,56 @@ def read_imu():
     prev_roll = 0
     prev_pitch = 0
     prev_yaw = 0
+    curr_roll = 0
+    curr_pitch = 0
+    curr_yaw = 0
     orientation = Point32()
+    orientation.x = 0
+    orientation.y = 0
+    orientation.z = 0
+    roll = 0
+    pitch = 0
+    yaw = 0
+
     ser.flush()
     imu_data_raw = ser.readline()
     imu_data = imu_data_raw.split(',')
-    if len(imu_data) > 19:
+
+    if len(imu_data) > 21:
+
         roll = float(imu_data[17])
         pitch = float(imu_data[18])
         yaw = float(imu_data[19])
 
-
     imu_pub = rospy.Publisher('/novatel_imu', Point32, queue_size=10)
     rospy.init_node ('novatel_imu', anonymous=True)
-    rate = rospy.Rate(10)
+    rate = rospy.Rate(20)
+
     while not rospy.is_shutdown():
-        ser.flush()
-        imu_data_raw = ser.readline()
-        imu_data = imu_data_raw.split(',')
-        if len(imu_data) > 19:
-           curr_roll = float(imu_data[17])
-           curr_pitch = float(imu_data[18])
-           curr_yaw = float(imu_data[19])
 
-           # fix to IMU buffer issue
-           if abs(curr_roll - prev_roll) > 20:
-               ser.flush()
-           if abs(curr_pitch - prev_pitch) > 20:
-               ser.flush()
-           if abs(curr_yaw - prev_yaw) > 20:
-               ser.flush()
 
-        orientation.x = roll
-        orientation.y = pitch
-        orientation.z = yaw
+        if ser.in_waiting:
 
-        rospy.loginfo(orientation)
-        imu_pub.publish(orientation)
-        rate.sleep()
+            ser.flush()
+
+            imu_data_raw = ser.readline()
+            imu_data = imu_data_raw.split(',')
+            prev_roll = curr_roll
+            prev_pitch = curr_pitch
+            prev_yaw = curr_yaw
+
+            if len(imu_data) > 20:
+
+               curr_roll = float(imu_data[17])
+               curr_pitch = float(imu_data[18])
+               curr_yaw = float(imu_data[19])
+
+            orientation.x = curr_roll
+            orientation.y = curr_pitch
+            orientation.z = curr_yaw
+
+            rospy.loginfo(orientation)
+            imu_pub.publish(orientation)
 
 
 if __name__ == '__main__':
